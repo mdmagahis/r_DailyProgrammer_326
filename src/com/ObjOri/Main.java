18907915919df06996ae56f2a18861fb43b54440
Mark Magahis <mark.magahis@gmail.com>
2017-08-13 22:18:36 -0500

    Adds code for main and checkPrime() method using brute force to evaluate primality

--- src/com/ObjOri/Main.java
+++ src/com/ObjOri/Main.java
@@ -3,6 +3,45 @@ package com.ObjOri;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int[] challengeInput = {270, 541, 993, 649};
+        for (int val:challengeInput){
+            boolean isPrime = false;
+            if (checkPrime(val)) isPrime = true;
+
+            if (isPrime){
+                System.out.println(val + " is prime.");
+            }
+            else {
+                int lessPrime = val;
+                int morePrime = val;
+
+                // Find nearest prime below val
+                while (!isPrime) isPrime = checkPrime(--lessPrime);
+
+                isPrime = false;
+
+                // Find nearest prime above val
+                while (!isPrime) isPrime = checkPrime(++morePrime);
+
+
+                System.out.println(lessPrime + " < " + val + " < " + morePrime);
+            }
+        }
+    }
+
+    // Brute force check if a number is prime
+    public static boolean checkPrime(int x){
+        // x
+        if (x < 4 && x >= 0)    return true;
+
+        // x is even
+        else if (x % 2 == 0)    return false;
+
+        // check every number up until x
+        for (int y = 3; y < x; y += 2) {
+            if (x % y == 0)    return false;
+        }
+
+        return true;
     }
 }
